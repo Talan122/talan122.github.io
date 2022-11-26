dfef661e7c4d0a0b61a7d2fc29449dbc69ce909a
Talan122 <63525338+Talan122@users.noreply.github.com>
2022-11-25 19:27:59 -0500

    batch thing

---
+++ batch/vid_to_gif.bat
@@ -0,0 +1,15 @@
+
+:run
+@echo off
+set /p videoInput="Input video -> "
+set /p gifOutput="Output name -> "
+set /p additionalArgs="Additional ffmpeg args -> "
+
+echo "Starting.."
+
+ffmpeg -i "%videoInput%" %additionalArgs% -filter_complex "[0:v] palettegen" palette.png
+ffmpeg -i "%videoInput%" %additionalArgs% -i palette.png -filter_complex "[0:v] fps=30,scale=480:-1 [new];[new][1:v] paletteuse" "%gifOutput%"
+
+del "palette.png"
+
+echo "Done!"
